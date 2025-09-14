48dc3eeea6fab89118049e49dceb2cdec9731eb5
iomegalinux1840 <dave.girard@gilbert-tech.com>
2025-09-14 19:54:07 -0400

    Fix landing page to show restaurants list instead of hardcoded redirect

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,8 +1,12 @@
 class LandingController < ApplicationController
   def index
     @restaurants = Restaurant.active.limit(10)
-    # For now, redirect to the default restaurant
-    # Later this will be a proper landing page
-    redirect_to '/r/la-bonne-cuisine'
+
+    # Show list of restaurants or redirect to first one if it exists
+    if @restaurants.any?
+      render html: "<h1>Menu Platform</h1><p>Available Restaurants:</p><ul>#{@restaurants.map { |r| "<li><a href='/r/#{r.slug}'>#{r.name}</a></li>" }.join}</ul>".html_safe
+    else
+      render html: "<h1>Menu Platform</h1><p>No restaurants available yet.</p>".html_safe
+    end
   end
 end
