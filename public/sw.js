28c0623ba50e730a9aadd5ad47c722028d84c73f
iomegalinux1840 <dave.girard@gilbert-tech.com>
2025-11-02 16:32:09 -0500

    Add stub service worker to satisfy browser request

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", () => {
+  // No offline cache; this file exists to prevent 404 errors from browsers requesting /sw.js
+});
